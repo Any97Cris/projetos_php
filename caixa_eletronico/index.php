538bed10c79e0d542421db96f1c6d4672001a3b6
Any97Cris <criscianysilva1997@gmail.com>
2023-02-07 10:32:53 -0400

    listar transacao

--- caixa_eletronico/index.php
+++ caixa_eletronico/index.php
@@ -36,6 +36,35 @@ if(isset($_SESSION['banco']) && empty($_SESSION['banco']) == false){
     <h1>Banco XYZ</h1>
     <p>Agência: <?php echo $info['conta_agencia'] ?></p>
     <p>Conta: <?php echo $info['conta_conta'] ?></p>
+    <p>Saldo: <?php echo $info['conta_saldo'] ?></p>
     <a href="logout.php">sair</a>
+    <hr>
+    <h3>Movimentações/Extrato</h3>
+    <a href="addCache.php">Adicionar Transação</a>
+    <br>
+    <br>
+    <table border="1" width="400">
+        <tr>
+            <th>Data</th>
+            <th>Valor</th>
+        </tr>
+        <?php 
+            $sql = $pdo->prepare("SELECT * FROM historico WHERE historico_conta_id = :id ");
+            $sql->bindValue(":id",$id);
+            $sql->execute();
+            foreach($sql->fetchAll() as $itens){            
+        ?>
+        <tr>
+            <td><?php echo date('d/m/Y H:i', strtotime($itens['historico_data_operacao'])) ?></td>
+            <td>
+                <?php if($itens['historico_tipo'] == '0'){ ?>
+                    <span style="color:green;"><?php echo $itens['historico_valor'] ?></span>
+                <?php }else{?>
+                    <span style="color:red;"><?php echo $itens['historico_valor'] ?></span>
+                <?php } ?>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
