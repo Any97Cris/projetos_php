427f0841546a864d5dc72998473338d6c8b21526
Any97Cris <criscianysilva1997@gmail.com>
2023-02-09 14:39:27 -0400

    tela inicial

--- conversorPalavraDigito/index.php
+++ conversorPalavraDigito/index.php
@@ -17,17 +17,46 @@
     </form>
     <?php
         $palavras = filter_input(INPUT_POST, 'palavras');
-        
-        $p = Array();
-        $p = Array($palavras);
-        
-        foreach($p as $pa){
-            switch(){
-                case 'um':
-                echo 1;
-                break;
+        if(!empty($palavras)){
+            $p = explode(",",$palavras);
+
+            $numeros = array();
+            
+            foreach($p as $ps){
+                switch($ps){
+                    case 'um':
+                        $numeros[] = 1;
+                        break;
+                    case 'dois':
+                        $numeros[] = 2;
+                        break;
+                    case 'tres':
+                        $numeros[] = 3;
+                        break;
+                    case 'quatro':
+                        $numeros[] = 4;
+                        break;
+                    case 'cinco':
+                        $numeros[] = 5;
+                        break;
+                    case 'seis':
+                        $numeros[] = 6;
+                        break;
+                    case 'sete':
+                        $numeros[] = 7;
+                        break;
+                    case 'oito':
+                        $numeros[] = 8;
+                        break;
+                    case 'nove':
+                        $numeros[] = 9;
+                        break;
+                    
+                }
             }
+            echo "<p>".implode(",", $numeros)."</p>";
         }
+
     ?>
 </body>
 </html>
