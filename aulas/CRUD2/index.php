1a9610d83f789039a2cdc42af9ef6c334d18a9c3
Any97Cris <criscianysilva1997@gmail.com>
2023-02-14 10:55:40 -0400

    pagina inicial crud2

---
+++ aulas/CRUD2/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once "Contato.php";
+$contato = new Contato();
+?>
+
+<h1>Contatos</h1>
+<a href="cadastrar.php">Cadastrar Contato</a>
+<hr>
+<table border="1">
+    <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Ações</th>
+    </tr>
+    <?php
+    $lista = $contato->getAll();
+    foreach($lista as $l){    
+    ?>
+        <tr>
+            <td><?php echo $l['nome'] ?></td>
+            <td><?php echo $l['email'] ?></td>
+            <td>
+                <a href="editar.php?id=<?php echo $l['id']?>">[ Editar ]</a>
+                <a href="deletar.php?id=<?php echo $l['id'] ?>">[ Deletar ]</a>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
