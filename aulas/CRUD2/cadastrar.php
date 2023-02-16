3b0033f0fb84694120b0b285592700ff274c2f1a
Any97Cris <criscianysilva1997@gmail.com>
2023-02-16 09:28:56 -0400

    form cadastrar

---
+++ aulas/CRUD2/cadastrar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+    <h1>Adicionar Usuário</h1>
+    <form action="cadastrar_submit.php" method="post">
+        <label>Nome: </label><br>
+        <input type="text" name="nome">
+        <br>
+        <label>Email: </label><br>
+        <input type="email" name="email">
+        <br>
+        <br>
+        <br>
+        <input type="submit" value="Adicionar">
+    </form>
+</body>
+</html>
