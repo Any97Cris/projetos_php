5e728956ca5c986adfdc3e1ad9fca99cb73778f0
Any97Cris <criscianysilva1997@gmail.com>
2023-02-17 08:48:52 -0400

    classe com metodos

--- aulas/PDOStatemant/usuarios.php
+++ aulas/PDOStatemant/usuarios.php
@@ -9,7 +9,40 @@ class Usuarios {
         }catch(PDOException $e){
             echo "Falha: {$e->getMessage()}";
         }
-        $this->pdo = new PDO();
+        
+    }
+
+    public function selecionar($id){
+
+        $sql = $this->pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        $array = array();
+        if($sql->rowCount() > 0){
+            $array = $sql->fetch();
+        }
+
+        return $array;
+    }
+
+    public function inserir($nome, $email, $senha){
+        $sql = $this->pdo->prepare("INSERT INTO usuarios SET nome = :nome, email = :email, senha = :senha");
+        $sql->bindParam("nome", $nome);
+        $sql->bindParam(":email", $email);
+        $sql->bindValue("senha", md5($senha));
+        $sql->execute();
+    }
+
+    public function atualizar($nome, $email, $senha, $id){
+        $sql = $this->pdo->prepare("UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?");
+        $sql->execute(array($nome, $email, md5($senha), $id));
+    }
+
+    public function excluir($id){
+        $sql = $this->pdo->prepare("DELETE FROM usuarios WHERE id = ?");
+        $sql->bindValue(1, $id);
+        $sql->execute();
     }
 
 }
