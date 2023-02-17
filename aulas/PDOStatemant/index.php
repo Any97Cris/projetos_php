ccaaee81f00f17789158748a10360dc84b16c512
Any97Cris <criscianysilva1997@gmail.com>
2023-02-17 08:48:44 -0400

    página principal

--- aulas/PDOStatemant/index.php
+++ aulas/PDOStatemant/index.php
@@ -1,4 +1,20 @@
 <?php
 require_once "usuarios.php";
 
+$u = new Usuarios();
+
+// $res = $u->selecionar(1);
+// echo "<pre>";
+// print_r($res);
+
+//$res = $u->inserir("Crisciany", "crisciany@gmail.com", "123456");
+
+//$res = $u->atualizar("Cris Souza", "criscianysouza@gmail.com", "Cris123", 3);
+
+$res = $u->excluir(3);
+
+// $senha = md5("Sam123");
+// echo $senha;
+
+
 ?>
